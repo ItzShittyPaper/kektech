3ccd6e656c5dce94ed4983ddd362d1845edbedca
M4kulatura <m4kulatura@gmail.com>
2023-04-01 22:21:50 +0200

    wasd now acts like arrows (conflict with A / S)

--- input.cpp
+++ input.cpp
@@ -4,20 +4,18 @@
 
 /* 
 
-KEYCODES:
-W = 0
-A = 1
-S = 2
-D = 3
-Z = 4
-X = 5
-S = 6
+== KEYCODES ==
+action:
+	Z = 0
+	X = 1
+	C = 2
+	V = 3
 
-ARROWS:
-	LEFT = 7
-	RIGHT = 8
-	UP = 9
-	DOWN = 10
+movement:
+	A / LEFT = 7
+	D / RIGHT = 8
+	W / UP = 9
+	S / DOWN = 10
 
 */
 
@@ -26,28 +24,22 @@ SDL_Event e;
 void I_ProcessKeys() {
 
 	switch(e.key.keysym.sym) {
-                case SDLK_w:
+
+		/* action keys */
+		case SDLK_z:
 			I_ProcessInput(0);
 			break;
-		case SDLK_a:
+		case SDLK_x:
 			I_ProcessInput(1);
 			break;
-		case SDLK_s:
+		case SDLK_c:
 			I_ProcessInput(2);
 			break;
-		case SDLK_d:
+		case SDLK_v:
 			I_ProcessInput(3);
 			break;
-		case SDLK_z:
-			I_ProcessInput(4);
-			break;
-		case SDLK_x:
-			I_ProcessInput(5);
-			break;
-		case SDLK_s:
-			I_ProcessInput(6);
-			break;
-	
+
+		/* movement keys, wasd and arrows, they basically do the same thing */
 		case SDLK_LEFT:
 			I_ProcessInput(7);
 			break;
@@ -60,7 +52,20 @@ void I_ProcessKeys() {
 		case SDLK_DOWN:
 			I_ProcessInput(10);
 			break;
+		case SDLK_w:
+			I_ProcessInput(7);
+			break;
+		case SDLK_a:
+			I_ProcessInput(8);
+			break;
+		case SDLK_s:
+			I_ProcessInput(9);
+			break;
+		case SDLK_d:
+			I_ProcessInput(10);
+			break;
 
+		/* fullscreen toggle */
 		case SDLK_F11:
 			ToggleFullscreen(window);
 			break;
@@ -75,37 +80,30 @@ bool I_ProcessInput(int key_code) {
 	switch(key_code) {
 
 		case 0:
-			std::cout << "W pressed" << std::endl;
-			break;
-		case 1:
-			std::cout << "A pressed" << std::endl;
-			break;
-		case 2:
-			std::cout << "S pressed" << std::endl;
-			break;
-		case 3:
-			std::cout << "D pressed" << std::endl;
-			break;
-		case 4:
 			std::cout << "Z pressed" << std::endl;
 			break;
-		case 5:
+		case 1:
 			std::cout << "X pressed" << std::endl;
 			break;
-		case 6:
-			std::cout << "S pressed" << std::endl;
+		case 2:
+			std::cout << "C pressed" << std::endl;
 			break;
+		case 3:
+			std::cout << "V pressed" << std::endl;
+			break;
+
+			
 		case 7:
-			std::cout << "LEFT_ARROW pressed" << std::endl;
+			std::cout << "LEFT / A pressed" << std::endl;
 			break;
 		case 8:
-			std::cout << "RIGHT_ARROW pressed" << std::endl;
+			std::cout << "RIGHT / D pressed" << std::endl;
 			break;
 	        case 9:
-			std::cout << "UP_ARROW pressed" << std::endl;
+			std::cout << "UP / W pressed" << std::endl;
 			break;
 	        case 10:
-			std::cout << "DOWN_ARROW pressed" << std::endl;
+			std::cout << "DOWN / S pressed" << std::endl;
 			break;
 	}
 	}

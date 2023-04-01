18e5d21df2131388bc3957afed918c42a294fe42
mcflurrymcs <77932726+mcflurrymcs@users.noreply.github.com>
2023-04-01 21:06:48 +0200

    Added W,S,D keys
    balls

--- input.cpp
+++ input.cpp
@@ -5,17 +5,19 @@
 /* 
 
 KEYCODES:
-
-Z = 0
-X = 1
-A = 2
-S = 3
+W = 0
+A = 1
+S = 2
+D = 3
+Z = 4
+X = 5
+S = 6
 
 ARROWS:
-	LEFT = 4
-	RIGHT = 5
-	UP = 6
-	DOWN = 7
+	LEFT = 7
+	RIGHT = 8
+	UP = 9
+	DOWN = 10
 
 */
 
@@ -24,31 +26,39 @@ SDL_Event e;
 void I_ProcessKeys() {
 
 	switch(e.key.keysym.sym) {
-
-		case SDLK_z:
+                case SDLK_w:
 			I_ProcessInput(0);
 			break;
-		case SDLK_x:
+		case SDLK_a:
 			I_ProcessInput(1);
 			break;
-		case SDLK_a:
+		case SDLK_s:
 			I_ProcessInput(2);
 			break;
-		case SDLK_s:
+		case SDLK_d:
 			I_ProcessInput(3);
+			break;
+		case SDLK_z:
+			I_ProcessInput(4);
+			break;
+		case SDLK_x:
+			I_ProcessInput(5);
+			break;
+		case SDLK_s:
+			I_ProcessInput(6);
 			break;
 	
 		case SDLK_LEFT:
-			I_ProcessInput(4);
+			I_ProcessInput(7);
 			break;
 		case SDLK_RIGHT:
-			I_ProcessInput(5);
+			I_ProcessInput(8);
 			break;
 		case SDLK_UP:
-			I_ProcessInput(6);
+			I_ProcessInput(9);
 			break;
 		case SDLK_DOWN:
-			I_ProcessInput(7);
+			I_ProcessInput(10);
 			break;
 
 		case SDLK_F11:
@@ -65,29 +75,38 @@ bool I_ProcessInput(int key_code) {
 	switch(key_code) {
 
 		case 0:
-			std::cout << "Z pressed" << std::endl;
+			std::cout << "W pressed" << std::endl;
 			break;
 		case 1:
-			std::cout << "X pressed" << std::endl;
-			break;
-		case 2:
 			std::cout << "A pressed" << std::endl;
 			break;
-		case 3:
+		case 2:
 			std::cout << "S pressed" << std::endl;
 			break;
-
+		case 3:
+			std::cout << "D pressed" << std::endl;
+			break;
 		case 4:
-			std::cout << "LEFT pressed" << std::endl;
+			std::cout << "Z pressed" << std::endl;
 			break;
 		case 5:
-			std::cout << "RIGHT pressed" << std::endl;
+			std::cout << "X pressed" << std::endl;
 			break;
 		case 6:
-			std::cout << "UP pressed" << std::endl;
+			std::cout << "S pressed" << std::endl;
 			break;
 		case 7:
-			std::cout << "DOWN pressed" << std::endl;
+			std::cout << "LEFT_ARROW pressed" << std::endl;
 			break;
+		case 8:
+			std::cout << "RIGHT_ARROW pressed" << std::endl;
+			break;
+	        case 9:
+			std::cout << "UP_ARROW pressed" << std::endl;
+			break;
+	        case 10:
+			std::cout << "DOWN_ARROW pressed" << std::endl;
+			break;
+	}
 	}
 }
